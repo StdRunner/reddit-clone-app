1eee2b28d869af41f4274df53633b57fdada3f1f
doyun <kjb119912@gmail.com>
2022-11-29 22:59:49 +0900

    - 섹션8 완료 : 포스트 나열(무한 로딩), 유저 페이지 추가, UI 수정

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -4,14 +4,48 @@ import Image from 'next/image'
 import React from 'react'
 import { Post } from '../types'
 import dayjs from 'dayjs'
+import { useAuthState } from '../context/auth'
+import axios from 'axios';
+import { useRouter } from 'next/router'
 
 interface PostCardProps {
     post: Post
+    subMutate?: () => void
 }
 
-const PostCard = ({ post: {
-    identifier, slug, title, body, subName, createdAt, voteScore, userVote, commentCount, url, username, sub
-} }: PostCardProps) => {
+const PostCard = ({ post: 
+    {
+        identifier, 
+        slug, 
+        title, 
+        body, 
+        subName, 
+        createdAt, 
+        voteScore, 
+        userVote, 
+        commentCount, 
+        url, 
+        username, 
+        sub
+    },
+    subMutate }: PostCardProps) => {
+    const router = useRouter();
+    const isInSubPage = router.pathname === '/r/[sub]';
+
+    const {authenticated}  = useAuthState();
+
+    const vote = async (value: number) => {
+        if(!authenticated) router.push("/login");
+
+        if(value === userVote) value = 0;
+
+        try {
+            await axios.post("/votes", { identifier, slug, value });
+            if(subMutate) subMutate();
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <div
@@ -23,7 +57,7 @@ const PostCard = ({ post: {
                 {/* 좋아요 부분 */}
                 <div
                     className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
-                    // onClick={() => vote(1)}
+                    onClick={() => vote(1)}
                 >
                     <i
                         className={classNames("fas fa-arrow-up", {
@@ -36,7 +70,7 @@ const PostCard = ({ post: {
                 {/* 싫어요 부분 */}
                 <div
                     className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
-                    // onClick={() => vote(-1)}
+                    onClick={() => vote(-1)}
                 >
                     <i
                         className={classNames("fas fa-arrow-down", {
@@ -49,37 +83,42 @@ const PostCard = ({ post: {
             
             {/* 포스트 데이터 부분 */}
             <div className='w-full p-2'>
-                {/* <div className='flex items-center'>
-                    <Link href={`/r/${subName}`}>
-                        <a>
-                            <Image
-                                src={sub!.imageUrl}
-                                alt="sub"
-                                className='rounded-full cursor-pointer'
-                                width={12}
-                                height={12}
-                            />
-                        </a>
-                    </Link>
-                    <Link href={`/r/${subName}`}>
-                        <a className='ml-2 text-xs font-bold cursor-pointer hover:underline'>
-                            /r/{subName}
-                        </a>
-                    </Link>
-                    <span className='mx-1 text-xs text-gray-400'>ㆍ</span>
-                </div> */}
-                
-                <p className='text-xs test-gray-400'>
-                    Posted by
-                    <Link href={`/r/${username}`}>
-                        <a className='mx-1 hover:underline'>/r/{username}</a>
-                    </Link>
-                    <Link href={url}>
-                        <a className='mx-1 hover:underline'>
-                            {dayjs(createdAt).format('YYYY-MM-DD HH:mm')}
-                        </a>
-                    </Link>
-                </p>
+                {/* 커뮤니티 페이지가 아닐 때 = path가 "/r/[sub]"가 아닐 때 */}
+                <div className='flex items-center'>
+                    {!isInSubPage && (
+                        <div className='flex items-center'>
+                            <Link href={`/r/${subName}`}>
+                                <a>
+                                    <Image
+                                        src={sub!.imageUrl}
+                                        alt="sub"
+                                        className='rounded-full cursor-pointer'
+                                        width={12}
+                                        height={12}
+                                    />
+                                </a>
+                            </Link>
+                            <Link href={`/r/${subName}`}>
+                                <a className='ml-2 text-xs font-bold cursor-pointer hover:underline'>
+                                    /r/{subName}
+                                </a>
+                            </Link>
+                            <span className='mx-1 text-xs text-gray-400'>ㆍ</span>
+                        </div>
+                    )}
+                    
+                    <p className='text-xs test-gray-400'>
+                        Posted by
+                        <Link href={`/u/${username}`}>
+                            <a className='mx-1 hover:underline'>/u/{username}</a>
+                        </Link>
+                        <Link href={url}>
+                            <a className='mx-1 hover:underline'>
+                                {dayjs(createdAt).format('YYYY-MM-DD HH:mm')}
+                            </a>
+                        </Link>
+                    </p>
+                </div>
             
                 <Link href={url}>
                     <a className='abc my-1 text-lg font-medium'>{title}</a>
--- client/src/pages/subs/create.tsx
+++ client/src/pages/subs/create.tsx
@@ -26,14 +26,14 @@ const SubCreate = () => {
     
     return (
         <div className='flex flex-col justify-center pt-16'>
-            <div className='w-10/12 mx-auto md:w-96'>
+            <div className='w-10/12 mx-auto md:w-96 bg-white rounded p-4'>
                 <h1 className=',b-2 text-lg font-medium'>
                     커뮤니티 만들기
                 </h1>
                 <hr />
                 <form onSubmit={handleSubmit}>
                     <div className='my=6'>
-                        <p className='font-medium'>Name</p>
+                        <p className='font-medium mt-5'>Name</p>
                         <p className='mb-2 text-xs text-gray-400'>
                             커뮤니티 이름은 변경할 수 없습니다.
                         </p>
@@ -45,7 +45,7 @@ const SubCreate = () => {
                         />
                     </div>
                     <div className='my=6'>
-                        <p className='font-medium'>Title</p>
+                        <p className='font-medium mt-5'>Title</p>
                         <p className='mb-2 text-xs text-gray-400'>
                             주제를 나타냅니다. 언제든지 변경할 수 있습니다.
                         </p>
@@ -57,7 +57,7 @@ const SubCreate = () => {
                         />
                     </div>
                     <div className='my=6'>
-                        <p className='font-medium'>Description</p>
+                        <p className='font-medium mt-5'>Description</p>
                         <p className='mb-2 text-xs text-gray-400'>
                             해당 커뮤니티에 대한 설명입니다.
                         </p>
@@ -70,7 +70,7 @@ const SubCreate = () => {
                     </div>
                     <div className='flex justify-end'>
                         <button
-                            className='px-4 py-1 text-sm font-semibold rounded text-white bg-gray-400 border'
+                            className='px-4 py-1 mt-5 text-sm font-semibold rounded text-white bg-gray-400 border'
                         >
                             커뮤니티 만들기
                         </button>
