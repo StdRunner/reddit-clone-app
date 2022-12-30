6879efc096a89f304aabfb01628f4251c2dffebd
doyun <kjb119912@gmail.com>
2022-12-30 22:00:42 +0900

    운영환경 배포를 위한 소스 및 설정 변경

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images:{
-    domains: ["www.gravatar.com", "localhost"]
+    domains: [
+      "www.gravatar.com", 
+      "localhost",
+      "ec2-3-86-23-163.compute-1.amazonaws.com"
+    ]
   }
 }
 
